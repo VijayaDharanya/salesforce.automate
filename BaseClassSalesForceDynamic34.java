5819618c1e3cb373ce65320f19c216af147b044f
VijayaDharanya <108997343+VijayaDharanya@users.noreply.github.com>
2022-08-22 19:03:24 +0530

    Add files via upload

---
+++ BaseClassSalesForceDynamic34.java
@@ -0,0 +1,54 @@
+package automate.test;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+
+public class BaseClassSalesForceDynamic34 {
+	public RemoteWebDriver driver;
+	public  ChromeDriver driver1;
+	@Parameters({"browserName","URL","uName","passWord","app"})
+	@BeforeMethod
+	public void preCondition(String browserName,String url,String uname,String pass,String app) {
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browserName.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(uname);
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(pass);
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+		driver.findElement(By.xpath("//div[contains(@class,'slds-icon-waffle')]")).click();
+		driver.findElement(By.xpath("//button[text()='View All']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Search apps or items...']")).sendKeys(app);
+	}
+
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+	}
+
+}
